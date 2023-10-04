99b8270d47234060793a9c6f77acbf5761a537d4
Botirjon <thedavlativich@gmail.com>
2023-10-04 13:12:45 +0500

    Created OrderHistory model

---
+++ crs/Training.Oqtepalavash.Clone.Backend/Project.Backend/Domain/Entity/OrderHistory.cs
@@ -0,0 +1,23 @@
+﻿using Project.Backend.Domain.Enums;
+
+namespace Project.Backend.Domain.Entity;
+
+public class OrderHistory
+{
+    public Guid UserId { get; set; }
+
+    public Product Order { get; set; }
+    public decimal Summa { get; set; }
+
+    public Status Status { get; set; }
+
+    public OrderHistory(){}
+
+    public OrderHistory(Guid userId, Product order, decimal summa, Status status)
+    {
+        UserId = userId;
+        Order = order;
+        Summa = summa;
+        Status = status;
+    }
+}
