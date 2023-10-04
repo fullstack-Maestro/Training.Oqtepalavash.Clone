4aa20339280360df9f828c918c40fc92418f4cf1
Botirjon <thedavlativich@gmail.com>
2023-10-04 13:03:32 +0500

    Created User model

--- crs/Training.Oqtepalavash.Clone.Backend/Project.Backend/Domain/Entity/User.cs
+++ crs/Training.Oqtepalavash.Clone.Backend/Project.Backend/Domain/Entity/User.cs
@@ -2,4 +2,26 @@
 
 public class User
 {
+    public Guid Id { get; set; }
+
+    public int RoleId { get; set; }
+    public string Name { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public DateTime? BirthDate { get; set; }
+
+    public List<Location>? Location;
+
+    public User(){}
+
+    public User(Guid id, int roleId, string name, string phoneNumber, DateTime? birthDate)
+    {
+        Id = id;
+        RoleId = roleId;
+        Name = name;
+        PhoneNumber = phoneNumber;
+        BirthDate = birthDate;
+    }
+
 }
