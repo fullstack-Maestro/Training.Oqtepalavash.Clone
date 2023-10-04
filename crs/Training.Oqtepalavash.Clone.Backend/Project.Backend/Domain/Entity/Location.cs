baea5ca31c72df040719d91b4393ea652365ba55
Ozodbek4 <anvarjonovozodbek416@gmail.com>
2023-10-04 13:12:54 +0500

    Created Location

---
+++ crs/Training.Oqtepalavash.Clone.Backend/Project.Backend/Domain/Entity/Location.cs
@@ -0,0 +1,19 @@
+﻿namespace Project.Backend.Domain.Entity;
+
+public class Location
+{
+    public Enum Hudud { get; set; }
+    public string Town { get; set; }
+    public string Street { get; set; }
+    public string Podyez { get; set; }
+
+    public Location() { }
+
+    public Location(Enum hudud, string town, string street, string podyez)
+    {
+        Hudud = hudud;
+        Town = town;
+        Street = street;
+        Podyez = podyez;
+    }
+}
