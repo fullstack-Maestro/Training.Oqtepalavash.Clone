650494078585c77ca1358a1aa02c5aafc89b0c55
Botirjon <thedavlativich@gmail.com>
2023-10-04 13:26:10 +0500

    created Branches

---
+++ crs/Training.Oqtepalavash.Clone.Backend/Project.Backend/Domain/Entity/Order.cs
@@ -0,0 +1,22 @@
+﻿namespace Project.Backend.Domain.Entity;
+
+public class Order
+{
+    public Guid Id { get; set; }
+
+    public Product ProductId { get; set; }
+
+    public int Count { get; set; }
+
+    public Order(){}
+
+    public Order(Guid id, Product productId, int count)
+    {
+        
+        Id = id;
+        
+        ProductId = productId;
+        
+        Count = count;
+    }
+}
